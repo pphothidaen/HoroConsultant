0e8065eb4c6f28012344d87adbc267cde8a90dfd
pphothidaen <pphothidaen@users.noreply.github.com>
2026-08-22 13:42:10 +0700

    Fix Azure deploy ingress and runtime URL

--- project/static/sw.js
+++ project/static/sw.js
@@ -5,7 +5,7 @@
  * Implements aggressive cache invalidation on new version deployments.
  */
 
-const CACHE_VERSION = 'v1.0.0.a85b028';
+const CACHE_VERSION = 'v1.0.0.cb5ea1e';
 const CACHE_NAME = `horoconsultant-${CACHE_VERSION}-cache`;
 const STATIC_ASSETS = [
   '/',
